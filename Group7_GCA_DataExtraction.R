e9ad0f5d94d402f2d810e14c7b89ecd7cca8d930
SenanHughes <senan_hughes@icloud.com>
2023-12-15 22:18:12 +0000

    Removed incorrect files, added required RNotebook and created Headings

--- Group7_GCA_DataExtraction.R
+++
@@ -1,7 +0,0 @@
-setwd("C:/Users/senan/OneDrive/Documents/Masters in Computing in Computer Animation/DAIE/GCA_GAD_and_VR_Group7")
-
-dataFromControl <- read.csv("GROUP_7_2023_GCA_RESULTS_CONTROL.csv")
-dataFromExperimental <- read.csv("GROUP_7_2023_GCA_RESULTS_EXPERIMENTAL.csv")
-
-summary(dataFromControl)
-summary(dataFromExperimental)
